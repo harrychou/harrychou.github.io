ca7697a85b475d8fcf9fc39dae3c1b0bba46b37f
Harry Chou <harrychou@gmail.com>
2015-04-22 00:18:22 -0400

    cool

---
+++ angularjs_5/app.js
@@ -0,0 +1,24 @@
+var app = angular.module("harrychou.grid", ['angularGrid']);
+
+app.controller('MainController', [function () {
+    var self = this;
+
+    var columnDefs = [
+    { displayName: "Make", field: "make" },
+    { displayName: "Model", field: "model" },
+    { displayName: "Price", field: "price" }
+    ];
+
+    var rowData = [
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Mondeo", price: 32000 },
+        { make: "Porsche", model: "Boxter", price: 72000 }
+    ];
+
+    self.gridOptions = {
+        columnDefs: columnDefs,
+        rowData: rowData,
+        dontUseScrolls: true // because so little data, no need to use scroll bars
+    };
+}]);
+
---
+++ angularjs_5/app2.js
@@ -0,0 +1,30 @@
+var app = angular.module("harrychou.grid", ['angularGrid']);
+
+app.controller('MainController', ['$http', function ($http) {
+    var self = this;
+
+    var columnDefs = [
+        { displayName: "Athlete", field: "athlete", width: 150 },
+        { displayName: "Age", field: "age", width: 90 },
+        { displayName: "Country", field: "country", width: 120 },
+        { displayName: "Year", field: "year", width: 90 },
+        { displayName: "Date", field: "date", width: 110 },
+        { displayName: "Sport", field: "sport", width: 110 },
+        { displayName: "Gold", field: "gold", width: 100 },
+        { displayName: "Silver", field: "silver", width: 100 },
+        { displayName: "Bronze", field: "bronze", width: 100 },
+        { displayName: "Total", field: "total", width: 100 }
+    ];
+
+    self.gridOptions = {
+        columnDefs: columnDefs,
+        rowData: null,
+    };
+
+    $http.get("olympicWinners.json")
+        .then(function (res) {
+            self.gridOptions.rowData = res.data;
+            self.gridOptions.api.onNewRows();
+        });
+}]);
+
